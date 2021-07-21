887712a8093157fe592966f3ced7945d6fef5c7f
Matthew Hinds <MatthewHinds@users.noreply.github.com>
2021-07-21 19:48:49 +1200

    Added creating of the Todo

--- Todo.Web.App/Program.cs
+++ Todo.Web.App/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Todo.Web.App
@@ -17,7 +13,7 @@ namespace Todo.Web.App
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:49159/") });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
 
             await builder.Build().RunAsync();
         }
